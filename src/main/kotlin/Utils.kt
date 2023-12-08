2204b2cbda1d28acd0d58e57671a221ddf04b935
Dragos Raducanu <raducanu.dragos@gmail.com>
2023-12-08 22:23:19 +0100

    adds solution for day 8

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,1 +1,5 @@
 fun String.trimSplit(delim: String): List<String> = this.trim().split(delim).filterNot { it.isEmpty() }
+
+fun lcm(ns: List<Long>): Long = ns.reduce { a, b ->
+    a * (b / a.toBigInteger().gcd(b.toBigInteger()).toLong())
+}
